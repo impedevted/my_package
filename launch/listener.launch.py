9c08e4ea44062ae579a6803a78f68fec1c45230e
Apiwat <apiwat.pitaksin@gmail.com>
2024-08-22 20:54:17 +0900

    fix typo

--- launch/listener.launch.py
+++ launch/listener.launch.py
@@ -4,5 +4,5 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(package='demo_nodes_cpp', 
-             executable='listner', )
+             executable='listener', )
     ])
